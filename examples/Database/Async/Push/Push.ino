ae71f0c4ba72da5e98d4ffe3186db4502b141dd8
unknown <k_suwatchai@hotmail.com>
2024-02-06 18:53:06 +0700

    Update examples.

--- examples/Database/Async/Push/Push.ino
+++ examples/Database/Async/Push/Push.ino
@@ -212,6 +212,9 @@ void setup()
     object_t arr("[]"); // initialize with [] to be used as array
     writer.join(arr, 4 /* no. of object_t (s) to join */, object_t("[12,34]"), object_t("[56,78]"), object_t(string_t("steve")), object_t(888));
 
+    // Note that value that sets to object_t other than JSON ({}) and Array ([]) can be valid only if it 
+    // used as array member value as above i.e. object_t(string_t("steve")) and object_t(888).
+
     // Push array
     database.push<object_t>(aClient, "/test/arr", arr, asyncCB);
 
--- examples/Database/Async/Set/Set.ino
+++ examples/Database/Async/Set/Set.ino
@@ -209,14 +209,17 @@ void setup()
     writer.create(obj3, "float/value", number_t(123.456, 2));
     writer.join(obj4, 3 /* no. of object_t (s) to join */, obj1, obj2, obj3);
     writer.create(json, "node/list", obj4);
-    
+
     // To print object_t
     // Serial.println(json);
-    
+
     database.set<object_t>(aClient, "/test/json", json, asyncCB);
 
     object_t arr("[]"); // initialize with [] to be used as array
     writer.join(arr, 4 /* no. of object_t (s) to join */, object_t("[12,34]"), object_t("[56,78]"), object_t(string_t("steve")), object_t(888));
+
+    // Note that value that sets to object_t other than JSON ({}) and Array ([]) can be valid only if it 
+    // used as array member value as above i.e. object_t(string_t("steve")) and object_t(888).
 
     // Set array
     database.set<object_t>(aClient, "/test/arr", arr, asyncCB);
