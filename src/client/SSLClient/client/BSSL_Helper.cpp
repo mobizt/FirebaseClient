f9d3bdd9f3d233c48f9c179020925896cb7c8837
Suwatchai K <k_suwatchai@hotmail.com>
2025-05-27 12:21:40 +0700

    Fix ESP32-C3 BearSSL compilation error

--- src/client/SSLClient/client/BSSL_Helper.cpp
+++ src/client/SSLClient/client/BSSL_Helper.cpp
@@ -1,6 +1,4 @@
 /*
-  Updated June 12, 2004.
-  
   WiFiClientBearSSL- SSL client/server for esp8266 using BearSSL libraries
   - Mostly compatible with Arduino WiFi shield library and standard
     WiFiClient/ServerSecure (except for certificate handling).
